16c6eadf9c1970e2748ed707cf9a7492b926765d
Sam Cohen <samuel.cohen@ufl.edu>
2024-10-10 14:13:06 -0400

    feat: allow for varying stalk dimensions + new test case

--- src/CellularSheaves.jl
+++ src/CellularSheaves.jl
@@ -2,27 +2,27 @@ module CellularSheaves
 
 export CellularSheaf, add_map!, coboundary_map, laplacian, is_global_section
 
-using LinearAlgebra  # Required for Matrix type
-using SparseArrays
 using BlockArrays
 
 struct CellularSheaf
-    V::Int    # Number of vertices
-    E::Int    # Number of edges
-    N::Int    # Max dimension of any stalk
+    V::Vector{Int}    # Number of vertices
+    E::Vector{Int}    # Number of edges
+    # N::Vector{Int}    # Max dimension of any stalk
     restriction_maps::BlockArray    # E*N x V*N matrix. Viewed as a block matrix with N*N blocks, the (e, v) entry represents the restriction map from v -> e.
 end
 
+
 # Constructor: no coboundary map given
-function CellularSheaf(V::Int, E::Int, N::Int)
-    restriction_maps = zeros(Float64, E * N, V * N)
+function CellularSheaf(V::Vector{Int}, E::Vector{Int})
+    restriction_maps = BlockArray{Float64}(zeros(sum(E), sum(V)), E, V)
+    return CellularSheaf(V, E, restriction_maps)
+end
 
-    row_blocks = fill(N, E)  # E blocks of N rows each
-    col_blocks = fill(N, V)  # V blocks of N columns each
-
-    # Create a BlockArray from restriction_maps
-    block_matrix = BlockArray(restriction_maps, row_blocks, col_blocks)
-    CellularSheaf(V, E, N, block_matrix)
+# Constructor: no coboundary map given, constant dimension
+function CellularSheaf(num_V::Int, num_E::Int, max_dimension::Int)
+    V = fill(max_dimension, num_V)  # E blocks of N rows each
+    E = fill(max_dimension, num_E)  # V blocks of N columns each
+    return CellularSheaf(V, E)
 end
 
 function add_map!(s::CellularSheaf, v::Int, e::Int, map::Matrix{Float64})
@@ -30,10 +30,11 @@ function add_map!(s::CellularSheaf, v::Int, e::Int, map::Matrix{Float64})
 end
 
 function coboundary_map(s::CellularSheaf)
+    # Iterate through the restriction_maps matrix and negate the second non-zero block in each row
     coboundary = copy(s.restriction_maps)
-    for e in 1:s.E
+    for e in eachindex(s.E)
         has_seen_block = false
-        for v in 1:s.V
+        for v in eachindex(s.V)
             if !has_seen_block && !(iszero(coboundary[Block(e, v)]))
                 has_seen_block = true
             elseif has_seen_block && !(iszero(coboundary[Block(e, v)]))
--- test/CellularSheaves.jl
+++ test/CellularSheaves.jl
@@ -24,43 +24,65 @@ test_arr = [15 2 2 4]   # V1 holds (1, 2) and V2 holds (2, 4)
 
 
 # Example sheaf 2: (a, b) -> b <- (b, c)
+# This is the same as example 1 except the vector space on the b edge has dimension 1, not 2
+# i.e. not all dimensions are the same (this is a more lightweight approach because we don't give the b edge an "extra" throwaway dimension)
+# Restriction maps go from b to b and all other elements to 0
+
+sheaf_2 = CellularSheaf([2, 2], [1])
+add_map!(sheaf_2, 1, 1, [0 1.])
+add_map!(sheaf_2, 2, 1, [1. 0]) 
+
+# Tests
+test_arr = [1 2 3 4]   # V1 holds (1, 2) and V2 holds (3, 4)
+@test !(laplacian(sheaf_2) * test_arr' ≈ [0, 0, 0, 0])
+
+test_arr = [1 2 2 4]   # V1 holds (1, 2) and V2 holds (2, 4)
+@test laplacian(sheaf_2) * test_arr' ≈ [0, 0, 0, 0]
+
+test_arr = [15 2 2 4]   # V1 holds (1, 2) and V2 holds (2, 4)
+@test laplacian(sheaf_2) * test_arr' ≈ [0, 0, 0, 0]
+
+
+
+# Example sheaf 3: (a, b) -> b <- (b, c)
 # Underlying graph: *-------*  (2 vertices, 1 edge)
 # a, b, and c are each 2-element vectors (unlike example sheaf 1)
 # Restriction maps go from b to b and all other elements to 0
 
-sheaf_2 = CellularSheaf(2, 1, 4)
-add_map!(sheaf_2, 1, 1, [0 0 1. 0; 0 0 0 1; 0 0 0 0; 0 0 0 0])
-add_map!(sheaf_2, 2, 1, [1. 0 0 0; 0 1 0 0; 0 0 0 0; 0 0 0 0]) 
+sheaf_3 = CellularSheaf(2, 1, 4)
+add_map!(sheaf_3, 1, 1, [0 0 1. 0; 0 0 0 1; 0 0 0 0; 0 0 0 0])
+add_map!(sheaf_3, 2, 1, [1. 0 0 0; 0 1 0 0; 0 0 0 0; 0 0 0 0]) 
 
 # Tests
 test_arr = [1 2 3 4 5 6 7 8]   # V1 holds (a =(1, 2), b =(3, 4)). V2 holds (b =(5, 6), c =(7, 8))
-@test !(laplacian(sheaf_2) * test_arr' ≈ zeros(8))
+@test !(laplacian(sheaf_3) * test_arr' ≈ zeros(8))
 
 test_arr = [1 2 3 4 3 4 7 8]   # V1 holds (a =(1, 2), b =(3, 4)). V2 holds (b =(3, 4), c =(7, 8))
-@test laplacian(sheaf_2) * test_arr' ≈ zeros(8)
+@test laplacian(sheaf_3) * test_arr' ≈ zeros(8)
 
 test_arr = [1 2 3 4 3 100 7 8]   # V1 holds (a =(1, 2), b =(3, 4)). V2 holds (b =(3, 100), c =(7, 8))
-@test !(laplacian(sheaf_2) * test_arr' ≈ zeros(8))
+@test !(laplacian(sheaf_3) * test_arr' ≈ zeros(8))
 
 
-# Example sheaf 3: Vertices are (a, b), (b, c), and (c, a). Edges connect the shared variables as in examples 1 and 2.
+
+# Example sheaf 4: Vertices are (a, b), (b, c), and (c, a). Edges connect the shared variables as in examples 1 and 2.
 # Underlying graph is a triangle
 # a, b, and c are each 1-element vectors
 
-sheaf_3 = CellularSheaf(3, 3, 2)
+sheaf_4 = CellularSheaf(3, 3, 2)
 
-add_map!(sheaf_3, 1, 1, [0 1.; 0 0])
-add_map!(sheaf_3, 2, 1, [1. 0; 0 0]) 
+add_map!(sheaf_4, 1, 1, [0 1.; 0 0])
+add_map!(sheaf_4, 2, 1, [1. 0; 0 0]) 
 
-add_map!(sheaf_3, 2, 2, [0 1.; 0 0])
-add_map!(sheaf_3, 3, 2, [1. 0; 0 0]) 
+add_map!(sheaf_4, 2, 2, [0 1.; 0 0])
+add_map!(sheaf_4, 3, 2, [1. 0; 0 0]) 
 
-add_map!(sheaf_3, 3, 3, [0 1.; 0 0])
-add_map!(sheaf_3, 1, 3, [1. 0; 0 0]) 
+add_map!(sheaf_4, 3, 3, [0 1.; 0 0])
+add_map!(sheaf_4, 1, 3, [1. 0; 0 0]) 
 
 # Tests
 test_arr = [10 20 20 30 30 50] # V1 holds (1, 2), V2 holds (20, 30), V3 holds (30, 50)
-@test !(laplacian(sheaf_3) * test_arr' ≈ zeros(6))
+@test !(laplacian(sheaf_4) * test_arr' ≈ zeros(6))
 
 test_arr = [10 20 20 30 30 10]   # V1 holds (1, 2), V2 holds (20, 30), V3 holds (30, 10)
-@test laplacian(sheaf_3) * test_arr' ≈ zeros(6)
+@test laplacian(sheaf_4) * test_arr' ≈ zeros(6)
