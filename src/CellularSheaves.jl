4284e225f32c38ffb7857bd40d8ab4697cba9baa
Sam Cohen <samuel.cohen@ufl.edu>
2024-10-10 12:47:10 -0400

    feat: incorporated ComponentArrays

--- src/CellularSheaves.jl
+++ src/CellularSheaves.jl
@@ -4,32 +4,33 @@ export CellularSheaf, add_map!, laplacian, is_global_section
 
 using LinearAlgebra  # Required for Matrix type
 using SparseArrays
+using BlockArrays
 
 struct CellularSheaf
     V::Int    # Number of vertices
     E::Int    # Number of edges
     N::Int    # Max dimension of any stalk
-    coboundary_map::Matrix    # E*N x V*N matrix. Viewed as a block matrix with N*N blocks, the (e, v) entry represents the restriction map from v -> e.
+    restriction_maps::BlockArray    # E*N x V*N matrix. Viewed as a block matrix with N*N blocks, the (e, v) entry represents the restriction map from v -> e.
 end
 
 # Constructor: no coboundary map given
 function CellularSheaf(V::Int, E::Int, N::Int)
-    CellularSheaf(V, E, N, zeros(Float64, E * N, V * N))
+    restriction_maps = zeros(Float64, E * N, V * N)
+
+    row_blocks = fill(N, E)  # E blocks of N rows each
+    col_blocks = fill(N, V)  # V blocks of N columns each
+
+    # Create a BlockArray from restriction_maps
+    block_matrix = BlockArray(restriction_maps, row_blocks, col_blocks)
+    CellularSheaf(V, E, N, block_matrix)
 end
 
 function add_map!(s::CellularSheaf, v::Int, e::Int, map::Matrix{Float64})
-    println("here")
-    dim = s.N
-    for i in 1:(dim)
-        for j in 1:(dim)
-            println((e - 1) * dim + i)
-            s.coboundary_map[(e - 1) * dim + i, (v - 1) * dim + j] = map[i, j];
-        end
-    end
+    s.restriction_maps[Block(e, v)] = map
 end
 
 function laplacian(s::CellularSheaf)
-    return s.coboundary_map' * s.coboundary_map
+    return s.restriction_maps' * s.restriction_maps
 end
 
 function is_global_section(s::CellularSheaf, v::Vector)
--- test/CellularSheaves.jl
+++ test/CellularSheaves.jl
@@ -9,16 +9,19 @@ using Test
 
 sheaf_1 = CellularSheaf(2, 1, 2)
 
+println(typeof(sheaf_1.restriction_maps))
+
+
 add_map!(sheaf_1, 1, 1, [0. 1.; 0. 0.]);
 add_map!(sheaf_1, 2, 1, [-1. 0.; 0. 0.])  # Note: Right now I'm manually negating the linear maps so that they work in the coboundary map. My next step
                                           # is to implement BlockArrays so that we can do automatic negating easily.
-println(sheaf_1.coboundary_map)
+println(sheaf_1.restriction_maps)
 
 test_arr = [1 2 3 4]   # V1 holds (1, 2) and V2 holds (3, 4)
-@test !(sheaf_1.coboundary_map * test_arr' ≈ [0, 0])
+@test !(sheaf_1.restriction_maps * test_arr' ≈ [0, 0])
 
 test_arr = [1 2 2 4]   # V1 holds (1, 2) and V2 holds (2, 4)
-@test sheaf_1.coboundary_map * test_arr' ≈ [0, 0]
+@test sheaf_1.restriction_maps * test_arr' ≈ [0, 0]
 
 test_arr = [15 2 2 4]   # V1 holds (1, 2) and V2 holds (2, 4)
-@test sheaf_1.coboundary_map * test_arr' ≈ [0, 0]
+@test sheaf_1.restriction_maps * test_arr' ≈ [0, 0]
