477beeb10a487aa41125b4c80e6ceac466ab6223
Sam Cohen <samuel.cohen@ufl.edu>
2024-10-10 13:08:32 -0400

    feat: function to make coboundary map from restriction maps

--- src/CellularSheaves.jl
+++ src/CellularSheaves.jl
@@ -1,6 +1,6 @@
 module CellularSheaves
 
-export CellularSheaf, add_map!, laplacian, is_global_section
+export CellularSheaf, add_map!, coboundary_map, laplacian, is_global_section
 
 using LinearAlgebra  # Required for Matrix type
 using SparseArrays
@@ -29,8 +29,25 @@ function add_map!(s::CellularSheaf, v::Int, e::Int, map::Matrix{Float64})
     s.restriction_maps[Block(e, v)] = map
 end
 
+function coboundary_map(s::CellularSheaf)
+    coboundary = copy(s.restriction_maps)
+    for e in 1:s.E
+        has_seen_block = false
+        for v in 1:s.V
+            if !has_seen_block && !(iszero(coboundary[Block(e, v)]))
+                has_seen_block = true
+            elseif has_seen_block && !(iszero(coboundary[Block(e, v)]))
+                coboundary[Block(e, v)] = -1 * coboundary[Block(e, v)]
+                break
+            end
+        end
+    end
+    return coboundary
+end
+
+
 function laplacian(s::CellularSheaf)
-    return s.restriction_maps' * s.restriction_maps
+    return coboundary_map(s)' * coboundary_map(s)
 end
 
 function is_global_section(s::CellularSheaf, v::Vector)
--- test/CellularSheaves.jl
+++ test/CellularSheaves.jl
@@ -8,20 +8,15 @@ using Test
 # Restriction maps go from b to b and all other elements to 0
 
 sheaf_1 = CellularSheaf(2, 1, 2)
+add_map!(sheaf_1, 1, 1, [0. 1.; 0. 0.])
+add_map!(sheaf_1, 2, 1, [1. 0.; 0. 0.]) 
 
-println(typeof(sheaf_1.restriction_maps))
-
-
-add_map!(sheaf_1, 1, 1, [0. 1.; 0. 0.]);
-add_map!(sheaf_1, 2, 1, [-1. 0.; 0. 0.])  # Note: Right now I'm manually negating the linear maps so that they work in the coboundary map. My next step
-                                          # is to implement BlockArrays so that we can do automatic negating easily.
-println(sheaf_1.restriction_maps)
-
+# Tests
 test_arr = [1 2 3 4]   # V1 holds (1, 2) and V2 holds (3, 4)
-@test !(sheaf_1.restriction_maps * test_arr' ≈ [0, 0])
+@test !(laplacian(sheaf_1) * test_arr' ≈ [0, 0, 0, 0])
 
 test_arr = [1 2 2 4]   # V1 holds (1, 2) and V2 holds (2, 4)
-@test sheaf_1.restriction_maps * test_arr' ≈ [0, 0]
+@test laplacian(sheaf_1) * test_arr' ≈ [0, 0, 0, 0]
 
 test_arr = [15 2 2 4]   # V1 holds (1, 2) and V2 holds (2, 4)
-@test sheaf_1.restriction_maps * test_arr' ≈ [0, 0]
+@test laplacian(sheaf_1) * test_arr' ≈ [0, 0, 0, 0]
