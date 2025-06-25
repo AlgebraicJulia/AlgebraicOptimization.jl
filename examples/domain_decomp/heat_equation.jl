39184d04122791b965f948c1fde10ffac806a667
James Fairbanks <fairbanksj@ufl.edu>
2025-06-25 12:41:39 -0400

    adding some diagnostic code

--- examples/domain_decomp/heat_equation.jl
+++ examples/domain_decomp/heat_equation.jl
@@ -17,9 +17,14 @@ bump(x, mu=0, sigma=10) = begin
 end
 
 
-rhs_func(x) = 3bump(x, 1 / 2, 1 / 19) - 5bump(x, -1 / 2, 1 / 20)
+rhs_func(x) = 3bump(x, 1 / 2, 1 / 20) - 3bump(x, -1 / 2, 1 / 20)
+# x, u = bvplin(1 / 20, x -> 0, x -> 0, x -> -rhs_func(x), [-1, 1], -1 / 2, -1 / 2, N)
+# x, u = bvplin(1 / 20, x -> 0, x -> 0, x -> -rhs_func(x), [-1, 1], 0,0, N)
+
+solveN = bvplin_solver(1 / 20, zero, zero, x -> -rhs_func(x), [-1, 1], N)
+x,u = solveN(0,0)
+
 p = plot(x, rhs_func.(x), label="b")
-x, u = bvplin(1 / 20, x -> 0, x -> 0, x -> -rhs_func(x), [-1, 1], -1 / 2, -1 / 2, N)
 p = plot!(p, x, u, label="bvplin_soln")
 
 
@@ -56,15 +61,18 @@ set_edge_maps!(s, 1, 2, 1, p1, p2)
 hp = CollocationHP([f1, f2], s)
 
 
-primal_sol, dual_sol = solve(hp, ADMM(2.0, 100))
+primal_sol, dual_sol = solve(hp, ADMM(2.0, 1))
 
 u1 = primal_sol[Block(1)]
 u2 = primal_sol[Block(2)]
 
 
-u_sol = vcat(u1[1:end-AB], u2)
+u_solA = vcat(u1[1:end-AB], u2)
+u_solB = vcat(u1, u2[AB+1:end])
+@show norm(u_solA - u_solB)
 
-p = plot(u_sol / norm(u_sol), label="hp")
-plot!(p, u / norm(u), color=:teal, label="krylov")
-#plot!(p, big_b / norm(big_b), color=:purple, label="b")
-p
+
+p = scatter!(p, x, u_solA, label="hpA")
+p = scatter!(p, x, u_solB, label="hpB")
+# plot!(p, u, color=:teal, label="reference")
+# plot!(p, rhs_func.(x), color=:purple, label="b")
