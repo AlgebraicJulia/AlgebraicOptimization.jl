53dade7f8d9be551cfa91caeae30a43f7d1f8454
James Fairbanks <fairbanksj@ufl.edu>
2025-07-01 15:37:49 -0400

    use linear interpolation instead of indexing for restrictions.

--- examples/domain_decomp/heat_equation.jl
+++ examples/domain_decomp/heat_equation.jl
@@ -1,10 +1,8 @@
 using AlgebraicOptimization
-using Graphs
 using SparseArrays
 using LinearAlgebra
-using Krylov
 using Plots
-using BlockArrays
+using DataInterpolations
 
 
 # SETUP
@@ -12,25 +10,27 @@ using BlockArrays
 
 include("fnc_utils.jl")
 
-N = 100 # Number of nodes in the global problem.
+N = 1500 # Number of nodes in the global problem.
 
 # GLOBAL PROBLEM SETUP AND SOLVE
 ################################
 
-# Construct 1st and 2nd differentiation matrices and a vector of nodes for the global problem.
-x, Dₓ, Dₓₓ = diffmat2(N - 1, (-1, 1))
 
 bump(x, mu=0, sigma=10) = begin
-    z = exp.(-(x .- mu) .^ 2 ./ sigma)
+    return exp.(-(x .- mu) .^ 2 ./ sigma)
 end
 
 
-rhs_func(x) = 2bump(x, 1 / 2, 1 / 20) - 4bump(x, -2 / 3, 1 / 30)
+uleft  = -1
+uright = +1
+rhs_func(x) = 2bump(x, 1 / 2, 1 / 20) - 4bump(x, -1 / 4, 1 / 20)
+
+# Construct 1st and 2nd differentiation matrices and a vector of nodes for the global problem.
+x, Dₓ, Dₓₓ = diffmat2(N - 1, (-1, 1))
 
 # Compute the correct global solution for comparison.
-solveN = bvplin_solver(1 / 20, zero, zero, x -> -rhs_func(x), [-1, 1], N-1)
+solveN = bvplin_solver(1 / 20, zero, zero, x -> -rhs_func(x), [uleft, uright], N-1)
 x, u = solveN(-1, 1)
-@show length(x), length(u)
 p = plot(x, rhs_func.(x), label="b", lw=3, title="Solution n=$N")
 p = plot!(p, x, u, label="bvplin_soln", lw=3, xlabel="x", ylabel="u", legend=:bottomright)
 
@@ -45,70 +45,49 @@ xAright = 0.1
 xBleft = -0.1
 AB = length(findall(xBleft .<= x .<= xAright))
 A = Nhalf + ceil(Int, AB / 2)
-# B = Nhalf + ceil(Int, AB / 2)-1
 B = N-A+AB
 @show length(x), N, A, B, A+B-AB, AB
 
 # Create the local solvers for each subdomain.
 solveA = bvplin_solver(1 / 20, zero, zero, x -> -rhs_func(x), [-1, xAright], A-1)
 solveB = bvplin_solver(1 / 20, zero, zero, x -> -rhs_func(x), [xBleft, 1], B-1)
-# solveAB = bvplin_solver(1 / 20, zero, zero, x -> -rhs_func(x), [xBleft, xAright], AB)
 
 # "wrap" a solver to just take in a u and return the projection of that u to the nearest solution.
 wrap_solver(solver) = u -> begin
     l = u[1]
     r = u[end]
     x, u = solver(l, r)
-    # @show length(x), length(u)
-    # @show minimum(x), maximum(x)
-    #return [l; u; r] #maybe you need to pad back the bcs
-    return u
+    return x, u
 end
 
 # Create wrapped local solvers.
 f1 = wrap_solver(solveA)
 f2 = wrap_solver(solveB)
-# f12 = wrap_solver(solveAB)
 
 
-"""
-Make restriction maps. In this case, they are both projections.
-"""
-function restriction_matrices(A, B, AB)
-    p1 = zeros(AB, A)
-    p1[1:AB, end-AB+1:end] .= I(AB)
-
-    p2 = zeros(AB, B)
-    p2[1:AB, 1:AB] .= I(AB)
-    return p1, p2
-end
-
-p1, p2 = restriction_matrices(A+1, B, AB)
 
 function alternating_projection(u₀, niter=1)
+    xa, ua = f1(zeros(A+1))
+    xb, ub = f2(zeros(B+1))
     function update(u1, u2)
-        u1[end] = u2[AB]
+        u2func = LinearInterpolation(u2, xb)
+        u1[end] = u2func(xAright)
         # u2_res = p2 * u2
         # u1[end-AB+1:end] .= u2_res
-        u1 = f1(u1)
+        xa, u1 = f1(u1)
+        u1func = LinearInterpolation(u1, xa)
         # u1_res = p1 * u1
         # u2[1:AB] .= u1_res
-        u2[1] = u1[end-AB]
-        u2 = f2(u2)
-        # mid = (u1_res + u2_res) / 2
-        # overlap_u = solveAB(mid[1], mid[end])[2]
+        u2[1] = u1func(xBleft)
+        xb, u2 = f2(u2)
         return u1, u2
     end
     u1 = u₀[1:A]
     u2 = u₀[N-B+1:end]
-    # display([u1[end-AB+1:end] u2[1:AB]])
-    # @assert norm(u1[end-(AB-1):end] - u2[1:AB]) < 1e-4
     for i in 1:niter
         u1, u2 = update(u1, u2)
     end
-    # @show length(u1) length(u2)
-    # @show u1[1], u2[1], u1[end], u2[end]
-    return vcat(u1[1:end-(AB)], u2)
+    return (xa, u1), (xb, u2)
 end
 
 # Plot both the solutions and the error over iterations
@@ -116,23 +95,28 @@ end
 begin
     rplt = plot(xlabel="x", ylabel="error", title="Error 1:$A, $(N-B):$N")
     rplt_tail = plot(xlabel="x", ylabel="error", title="Error 1:$A, $(N-B):$N")
-    iters = [1, 5, 10, 50, 100, 200, 500]
+    iters = [1, 5, 10, 25, 50, 100,200]
+    # iters = [50, 100, 200, 500]
     for i in iters
-        # ualt = alternating_projection(ones(N+1), i)
-        ualt = alternating_projection(u, i)
-        # ualt = alternating_projection(x, i)
-        plot!(p, x, ualt, label="ualt_$i", linestyle=:dash, lw=2)
+        # (xa, u1),(xb, u2) = alternating_projection(u, i)
+        (xa, u1),(xb, u2) = alternating_projection(collect(LinRange(uleft,uright,N+1)), i)
+        ualt_func(x) = x < xAright ? LinearInterpolation(u1,xa)(x) : LinearInterpolation(u2, xb)(x)
+
+        ualt = ualt_func.(x)
+        plot!(p, xa, u1, label="ualt_$i(a)", linestyle=:dash, lw=2)
+        plot!(p, xb, u2, label="ualt_$i(b)", linestyle=:dash, lw=2)
         if i < 100
             plot!(rplt, x, ualt - u, label="resid_$i", lw=2, ls=:dash)
         else
-            scatter!(rplt_tail, x[2:end], ((ualt - u) ./ u)[2:end], label="resid_$i", lw=2, ls=:dash)
+            scatter!(rplt_tail, x, log.(abs.((ualt - u) ./ (u .+ eps(Float64)))), label="resid_$i", lw=2, ls=:dash)
         end
-        res = norm(ualt - u)
-        relres = norm((ualt - u)./u)
-        # println("Residual of ualt_$i: $res\t $relres")
+        res = norm(ualt - u)/sqrt(N)
+        relres = norm((ualt - u)./u)/sqrt(N)
+        println("MSE of ualt_$i: $res\t Relative MSE $relres")
     end
     vline!(p, [xBleft, xAright], linestyle=:dash)
     vline!(rplt, [xBleft, xAright], linestyle=:dash)
+    vline!(rplt_tail, [xBleft, xAright], linestyle=:dash)
     plt = plot(p, rplt, rplt_tail, layout=[1; 1; 1], size=(800, 700))
 end
 plt
@@ -179,35 +163,3 @@ p = scatter!(p, x, u_solB, label="hp-fp")=#
 
 #u1 = f1(primal_sol[Block(1)])
 #u2 = f2(primal_sol[Block(2)])
-
-using Printf
-
-# local solutions
-u1 = f1(u[1:A])                 
-u2 = f2(u[N-B+1:end])           
-
-# corresponding meshes
-xA, _, _ = diffmat2(A - 1, (x[1], xAright))       
-xB, _, _ = diffmat2(B - 1, (xBleft, x[end]))        
-
-# Extract last 20 of A and first 20 of B
-n = ceil(Int, N/10)*2
-println("n = $n")
-xA_20 = xA[end-n+1:end]
-u1_20 = u1[end-n+1:end]
-
-xB_20 = xB[1:n]
-u2_20 = u2[1:n]
-
-# Header
-println("\n|   xA    |   u1(x)   ||   xB    |   u2(x)   |")
-println("--------------------------------------------------")
-
-# Print aligned
-for i in 1:n
-    xa = @sprintf("%7.4f", xA_20[i])
-    ua = @sprintf("%8.5f", u1_20[i])
-    xb = @sprintf("%7.4f", xB_20[i])
-    ub = @sprintf("%8.5f", u2_20[i])
-    println("| $xa | $ua || $xb | $ub |")
-end
