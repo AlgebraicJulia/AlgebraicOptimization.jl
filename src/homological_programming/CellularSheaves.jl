a23809c75c5fbaf7f5e8a1a1f5152df265ca85d8
Tyler Hanks <teh.hanks@gmail.com>
2025-08-05 13:10:10 -0400

    Implement coordinate descent

--- src/homological_programming/CellularSheaves.jl
+++ src/homological_programming/CellularSheaves.jl
@@ -1,7 +1,7 @@
 module CellularSheaves
 
 export AbstractCellularSheaf, CellularSheaf, PotentialSheaf, nearest_section, set_edge_maps!, Laplacian, apply_Laplacian, coboundary_map, apply_coboundary_map,
-    potential_objective, is_global_section
+    potential_objective, is_global_section, neighbor_edges
 
 using BlockArrays
 using SparseArrays
@@ -9,21 +9,72 @@ using LinearOperators
 using Krylov
 using LinearAlgebra
 using Graphs
+import Graphs
 using ForwardDiff
 using MLStyle: @match
 
 abstract type AbstractCellularSheaf end
-
+#=
 struct CellularSheaf <: AbstractCellularSheaf
     vertex_stalks::Vector{Int}
     edge_stalks::Vector{Int}
     coboundary::BlockArray
+    underlying_g::Graph
 end
 
 function CellularSheaf(vertex_stalks::Vector{Int}, edge_stalks::Vector{Int})
     cb = BlockArray(spzeros(sum(edge_stalks), sum(vertex_stalks)), edge_stalks, vertex_stalks)
 
-    return CellularSheaf(vertex_stalks, edge_stalks, cb)
+    return CellularSheaf(vertex_stalks, edge_stalks, cb, Graph(length(vertex_stalks)))
+end=#
+struct SheafEdge
+    v1::Int
+    v2::Int
+    rm12::AbstractArray
+    rm21::AbstractArray
+end
+
+struct CellularSheaf <: AbstractCellularSheaf
+    vertex_stalks::Vector{Int}
+    edge_stalks::Vector{Int}
+    edges::Vector{SheafEdge}
+    underlying_graph::Graph
+    edge_map::Dict{Tuple{Int,Int},Int}
+end
+
+function CellularSheaf(v_stalks::Vector{Int})
+    return CellularSheaf(
+        v_stalks, Int[], SheafEdge[], Graphs.Graph(length(v_stalks)), Dict{Tuple{Int,Int},Int}()
+    )
+end
+
+function neighbor_edges(s::CellularSheaf, v::Int)::Vector{SheafEdge}
+    ns = neighbors(underlying_graph(s), v)
+    res = SheafEdge[]
+
+    for w in ns
+        edge_idx = s.edge_map[(v, w)]
+        push!(res, s.edges[edge_idx])
+    end
+    return res
+end
+
+function set_edge_maps!(s::CellularSheaf, v1::Int, v2::Int, rm12::AbstractMatrix, rm21::AbstractMatrix)
+    #@assert size(rm12) == (s.edge_stalks[e], s.vertex_stalks[v1])
+    #@assert size(rm21) == (s.edge_stalks[e], s.vertex_stalks[v2]) TODO: make this correct
+    #s.coboundary[Block(e), Block(v1)] = rm1
+    #s.coboundary[Block(e), Block(v2)] = -rm2
+    @assert size(rm12)[1] == size(rm21)[1]
+    add_edge!(underlying_graph(s), v1, v2)
+    e = SheafEdge(v1, v2, rm12, rm21)
+    push!(s.edges, e)
+    s.edge_map[(v1, v2)] = length(s.edges)
+    s.edge_map[(v2, v1)] = length(s.edges)
+    push!(s.edge_stalks, size(rm12)[1])
+end
+
+function underlying_graph(s::AbstractCellularSheaf)
+    return s.underlying_graph
 end
 
 struct PotentialSheaf <: AbstractCellularSheaf
@@ -38,6 +89,7 @@ function PotentialSheaf(vertex_stalks::Vector{Int}, edge_stalks::Vector{Int}, po
 
     return PotentialSheaf(vertex_stalks, edge_stalks, cb, potentials)
 end
+
 
 function potential_objective(s::PotentialSheaf)
     total_potential(y) = sum([potential(y[Block(e)]) for (e, potential) in enumerate(s.potentials)])
@@ -55,7 +107,12 @@ function set_edge_maps!(s::AbstractCellularSheaf, v1::Int, v2::Int, e::Int, rm1:
     @assert size(rm2) == (s.edge_stalks[e], s.vertex_stalks[v2])
     s.coboundary[Block(e), Block(v1)] = rm1
     s.coboundary[Block(e), Block(v2)] = -rm2
+    add_edge!(underlying_graph(s), v1, v2)
 end
+
+
+
+
 
 """     nearest_section(s::CellularSheaf, x)
 
--- src/homological_programming/CoordinateDescent.jl
+++ src/homological_programming/CoordinateDescent.jl
@@ -13,7 +13,7 @@ using Optim
 using Plots
 using AlgebraicOptimization
 using Test
-
+using ForwardDiff
 
 # Let's start by defining a simple example problem to test our implementation.
 
@@ -28,61 +28,29 @@ for i in 1:overlap_dim
     R[i, i] = 1.0 # Identity in the overlap
 end
 
-s = @cellular_sheaf R begin
+#=s = @cellular_sheaf R begin
     x::Stalk{10}, y::Stalk{10} # Unrelated TODO: apparently this macro can't take variables as arguments, so we have to use a constant here
 
     R(x) == R(y)
-end
+end=#
 
-# Ok now let's define the coordinate descent algorithm
-HP = HomologicalProgram{Function,CellularSheaf}
+s = CellularSheaf([subproblem_dim, subproblem_dim])
+set_edge_maps!(s, 1, 2, R, R)
 
-# This code has the right form, but we need to update the optimization step to account for the equality constraints
-# coming from the neighboring nodes. Until we do that, the test at the end will fail.
-function block_coordinate_descent(p::HP, max_iters::Int=10, tol::Float64=1e-6)
-    # Initialize the solution
-    x = BlockArray(zeros(sum(p.sheaf.vertex_stalks)), p.sheaf.vertex_stalks)
-
-    for iter in 1:max_iters
-
-        x_prev = deepcopy(x) # Store the previous solution for convergence check
-
-        # Iterate over each node in the sheaf
-        for i in 1:length(p.objectives)
-            # Solve the subproblem for node i
-            objective = p.objectives[i]
-            # This code has the right outline, but this step needs to be modified
-            # to incorporate the equality constraint coming from the neighboring nodes.
-
-            # Joana, if you want some good Julia coding practice, you can implement the equality
-            # constrained Newton's method from chapter 10 of Boyd's Convex Optimization book for solving
-            # problems of the form:
-            #   min_x f(x) s.t. Ax = b
-            # where f is a smooth convex function, A is a matrix, and b is a vector.
-
-            res = optimize(objective, x[Block(i)], LBFGS(); autodiff=:forward)
-            x[Block(i)] = Optim.minimizer(res)
-        end
-
-        # Check convergence
-        if norm(x - x_prev) < tol
-            println("Converged after $iter iterations")
-            break
-        end
-    end
-
-    return x
-end
-
-function newton_equality_constraint(f, ∇f, ∇²f, A; x0=randn(size(A, 2)), ϵ=1e-6, max_iters=5, step_size)
+function newton_equality_constraint(f, A, b; x0=randn(size(A, 2)), ϵ=1e-6, max_iters=5, step_size=1)
     x = x0
+    trace = [x]
+
+    #println(x0)
 
     for iter in 1:max_iters
-        g = ∇f(x)
-        H = ∇²f(x)
+        g = ForwardDiff.gradient(f, x)
+        H = ForwardDiff.hessian(f, x)
 
-        KKT = [H A'; A zeros(size(A,1), size(A,1))]   
-        rhs = [-g; zeros(size(A,1))]
+        KKT = [H A'; A zeros(size(A, 1), size(A, 1))]
+        #rhs = [-g; zeros(size(A, 1))]
+        rhs = [-g; -A * x + b]
+
 
         sol = KKT \ rhs
         Δx = sol[1:length(x)]
@@ -95,11 +63,75 @@ function newton_equality_constraint(f, ∇f, ∇²f, A; x0=randn(size(A, 2)), ϵ
         end
 
         x += step_size * Δx
+        push!(trace, x)
     end
 
     println("No convergence")
     return x
 end
+
+
+
+# Ok now let's define the coordinate descent algorithm
+HP = HomologicalProgram{Function,CellularSheaf}
+
+# This code has the right form, but we need to update the optimization step to account for the equality constraints
+# coming from the neighboring nodes. Until we do that, the test at the end will fail.
+function block_coordinate_descent(p::HP, max_iters::Int=10, tol::Float64=1e-6)
+    # Initialize the solution
+    x = BlockArray(ones(sum(p.sheaf.vertex_stalks)), p.sheaf.vertex_stalks)
+
+    for iter in 1:max_iters
+        r = 2^iter - 1
+        x_prev = deepcopy(x) # Store the previous solution for convergence check
+
+        # Iterate over each node in the sheaf
+        for i in 1:length(p.objectives)
+            # Solve the subproblem for node i
+            objective = p.objectives[i]
+            # This code has the right outline, but this step needs to be modified
+            # to incorporate the equality constraint coming from the neighboring nodes.
+
+            # Build the A matrix and b vector to pass to Newton's
+            ns = neighbor_edges(p.sheaf, i)
+            As = AbstractArray[]
+            bs = Float64[]
+            A, b = undef, undef
+            for e in ns
+                if e.v1 == i
+                    A = e.rm12
+                    B = e.rm21
+                    y = x[Block(e.v2)]
+                    b = B * y
+                else
+                    A = e.rm21
+                    B = e.rm12
+                    y = x[Block(e.v1)]
+                    b = B * y
+                end
+                push!(As, A)
+                bs = vcat(bs, b)
+            end
+            bigA = vcat(As...)
+
+            #sol = newton_equality_constraint(objective, bigA, bs)
+            f(x) = objective(x) + r * norm(bigA * x - bs)^2
+
+            sol = optimize(f, x[Block(i)], LBFGS(); autodiff=:forward).minimizer
+            x[Block(i)] = sol
+        end
+
+        # Check convergence
+        if norm(x - x_prev) < tol
+            println("Converged after $iter iterations")
+            break
+        end
+    end
+
+    return x
+end
+
+
 
 # Test on our example problem
 objective1(x) = (x .- 1.0)' * (x .- 1.0) # Simple quadratic objective for the first subproblem
@@ -111,5 +143,5 @@ p = HP([objective1, objective2], s)
 solution = block_coordinate_descent(p)
 
 # Test that we get a global section as the solution
-is_global_section(p.sheaf, solution)
+#is_global_section(p.sheaf, solution)
 
