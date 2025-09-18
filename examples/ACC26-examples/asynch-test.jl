f3ca79c1f52bca6ebdf4a608fc31356d5dbc13af
tylerhanks <teh.hanks@gmail.com>
2025-09-17 20:57:02 -0400

    Trivial example

--- examples/ACC26-examples/asynch-test.jl
+++ examples/ACC26-examples/asynch-test.jl
@@ -7,10 +7,10 @@ using BlockArrays
 using Graphs
 using Distributions
 
-Random.seed!(1)
+Random.seed!(69)
 
 # Build a sheaf with a circle topology
-n_agents = 20
+n_agents = 2
 #C = [1.0 0.0 0.0 0.0; 0.0 0.0 1.0 0.0]
 C = rand(2, 4)
 s = EuclideanSheaf{Float64}(repeat([4], n_agents))
@@ -34,12 +34,15 @@ for i in 2:n_agents
 end
 add_sheaf_edge!(s, 1, n_agents, rand(2, 4), rand(2, 4))=#
 
-rm_generator(stalk_dim) = rand(2, stalk_dim)
+#rm_generator(stalk_dim) = rand(2, stalk_dim)
+rm_generator(stalk_dim) = Matrix{Float64}(I(stalk_dim))
 
-g = random_regular_graph(n_agents, 4; seed=69)
-#g = path_graph(n_agents)
-#g = erdos_renyi(n_agents, 0.3)
-println(is_connected(g))
+#g = random_regular_graph(n_agents, 4)
+g = path_graph(n_agents)
+#g = erdos_renyi(n_agents, 0.2)
+#g = complete_graph(n_agents)
+#g = wheel_graph(n_agents)
+#println(is_connected(g))
 
 s = sheaf_from_graph(g, 4, rm_generator)
 
@@ -48,12 +51,8 @@ L = Array(sparse(sheaf_laplacian_matrix(s)))
 # Test standard synchronous gradient descent convergence with theoretically optimal stepsize.
 K = opnorm(L, 2)
 
-B = 200
 
 
-γ_synch = 1 / K
-#γ_synch = 1
-γ_asynch = 2 / (K * (1 + 2 * (sqrt(n_agents) * B)))
 
 energy_function(L) = x -> 0.5 * x' * (L * x)
 
@@ -63,7 +62,10 @@ function compute_trajectory(L, x0, γ; max_iters=1000, tol=1e-8)
     x_curr = x0
     for i in 1:max_iters
         x_curr = x_curr - γ * (L * x_curr)
-        push!(losses, f(x_curr))
+
+        if i % (0.1 * max_iters) == 0
+            push!(losses, f(x_curr))
+        end
         if f(x_curr) < tol
             break
         end
@@ -75,26 +77,45 @@ function compute_trajectory_asynch(L, x0, γ, nblocks, block_size; max_iters=100
     f = energy_function(L)
     global_state = BlockArray(x0, repeat([block_size], nblocks))
     local_states = BlockArray(hcat([global_state for _ in 1:nblocks]...), repeat([block_size], nblocks), ones(Int, nblocks))
-    d1 = Normal(0.2 * B, 0.1 * B)
-    d2 = Normal(0.8 * B, 0.1 * B)
+    d1 = Normal(0.1 * B, 0.01 * B)
+    d2 = Normal(0.8 * B, 0.05 * B)
 
-    mixture = MixtureModel([d1, d2], [0.6, 0.4])
+    mixture = MixtureModel([d1, d2], [0.0, 1.0])
 
-    periods = rand(mixture, nblocks)
-    periods = round.(Int, periods)
-    println(periods)
-    phases = [rand(0:periods[i]-1) for i in 1:nblocks]
+    #periods = rand(mixture, nblocks)
+    #periods = ceil.(Int, periods)
+    #println(periods)
+    #phases = [rand(0:periods[i]-1) for i in 1:nblocks]
+    #broadcast_probs = rand(0:0.001:0.3, nblocks)
+    #update_probs = rand(nblocks)
+
+    # Dumb test case
+    periods = [B, B]
+    phases = [0, 0]
+
     losses = [f(x0)]
 
     for t in 1:max_iters
+        # Test that local states are stale i.e. columns have different values
+        #=if t % 1000 == 0
+            stale = true
+            for i in 1:nblocks
+                stale &= norm(local_states[:, Block(i)] - global_state) > 1e-4
+            end
+            println(stale)
+        end=#
+
         # every agent computes a local update
         g = BlockArray(L * local_states, repeat([block_size], nblocks), ones(Int, nblocks))
         for i in 1:nblocks
+            #if rand() < update_probs[i]
+            # update local state
+            local_states[Block(i), Block(i)] -= γ * g[Block(i), Block(i)]
+            # update global state
+            global_state[Block(i)] = local_states[Block(i), Block(i)][:]
+            #end
             if t % periods[i] == phases[i]
-                # update local state
-                local_states[Block(i), Block(i)] -= γ * g[Block(i), Block(i)]
-                # update global state
-                global_state[Block(i)] = local_states[Block(i), Block(i)][:]
+                #if rand() < broadcast_probs[i]
                 # if it's the right time, broadcast your local state to other agents
                 x = local_states[Block(i), Block(i)]
                 local_states[Block(i), :] .= x
@@ -103,19 +124,47 @@ function compute_trajectory_asynch(L, x0, γ, nblocks, block_size; max_iters=100
                 end=#
             end
         end
-        push!(losses, f(global_state))
-        if losses[end] < tol
-            break
+        #println(local_states)
+        if t % (0.1 * max_iters) == 0
+            push!(losses, f(global_state))
         end
+        #=if losses[end] < tol
+            break
+        end=#
     end
     return losses, global_state
 end
 
-x0 = rand(-2:0.01:2, 4 * n_agents)
 
-T = 100000
-energies_synch, final_state_synch = compute_trajectory(L, x0, γ_synch; max_iters=T)
-energies_asynch, final_state_asynch = compute_trajectory_asynch(L, x0, γ_synch, n_agents, 4; B=B, max_iters=T)
+T = Int(5e3)
+B = 30
 
-plot(energies_synch, yscale=:log10, title="Synch vs Asynch", xlabel="t", ylabel="Energy", label="Synch")
-plot!(energies_asynch, yscale=:log10, label="Asynch")
+γ_synch = 1 / K
+#γ_synch = 1
+γ_asynch = 2 / (K * (1 + 2 * (sqrt(n_agents) * B)))
+
+x0 = vcat(repeat([5.0], 4)..., repeat([15.0], 4)...)
+
+energies_synch, final_state_synch = compute_trajectory_asynch(L, x0, γ_synch, n_agents, 4; B=B, max_iters=T)
+energies_asynch, final_state_asynch = compute_trajectory_asynch(L, x0, γ_asynch, n_agents, 4; B=B, max_iters=T)
+
+plt = plot(energies_synch, title="Synch SS vs Asynch SS", xlabel="t", ylabel="Energy", label="1/K")
+plot!(plt, energies_asynch, label="2/(K(1+2√(n)B))")
+
+function run_experiments(T)
+    plts = []
+    for i in 1:20
+
+        x0 = rand(-5:0.01:5, 4 * n_agents)
+
+        energies_synch, final_state_synch = compute_trajectory(L, x0, γ_synch; max_iters=T)
+        energies_asynch, final_state_asynch = compute_trajectory_asynch(L, x0, γ_synch, n_agents, 4; B=B, max_iters=T)
+
+        plt = plot(energies_synch, yscale=:log10, title="Synch vs Asynch", xlabel="t", ylabel="Energy", label="Synch")
+        plot!(plt, energies_asynch, yscale=:log10, label="Asynch")
+        push!(plts, plt)
+    end
+    return plts
+end
+plt
+#plts = run_experiments(T);
