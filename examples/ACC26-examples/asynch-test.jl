04911e853453c96fb2168482b722f8d152792f40
tylerhanks <teh.hanks@gmail.com>
2025-09-19 16:18:08 -0400

    Test blocksparsearrays

--- examples/ACC26-examples/asynch-test.jl
+++ examples/ACC26-examples/asynch-test.jl
@@ -7,10 +7,10 @@ using BlockArrays
 using Graphs
 using Distributions
 
-Random.seed!(69)
+Random.seed!(1234)
 
 # Build a sheaf with a circle topology
-n_agents = 2
+n_agents = 20
 #C = [1.0 0.0 0.0 0.0; 0.0 0.0 1.0 0.0]
 C = rand(2, 4)
 s = EuclideanSheaf{Float64}(repeat([4], n_agents))
@@ -34,11 +34,12 @@ for i in 2:n_agents
 end
 add_sheaf_edge!(s, 1, n_agents, rand(2, 4), rand(2, 4))=#
 
-#rm_generator(stalk_dim) = rand(2, stalk_dim)
-rm_generator(stalk_dim) = Matrix{Float64}(I(stalk_dim))
+rm_generator(stalk_dim) = rand(2, stalk_dim)
+#rm_generator(stalk_dim) = Matrix{Float64}(I(stalk_dim))
 
 #g = random_regular_graph(n_agents, 4)
-g = path_graph(n_agents)
+g = dorogovtsev_mendes(n_agents)
+#g = path_graph(n_agents)
 #g = erdos_renyi(n_agents, 0.2)
 #g = complete_graph(n_agents)
 #g = wheel_graph(n_agents)
@@ -46,7 +47,8 @@ g = path_graph(n_agents)
 
 s = sheaf_from_graph(g, 4, rm_generator)
 
-L = Array(sparse(sheaf_laplacian_matrix(s)))
+#L = Array(sparse(sheaf_laplacian_matrix(s)))
+L = sheaf_laplacian_matrix(s)
 
 # Test standard synchronous gradient descent convergence with theoretically optimal stepsize.
 K = opnorm(L, 2)
@@ -63,9 +65,9 @@ function compute_trajectory(L, x0, γ; max_iters=1000, tol=1e-8)
     for i in 1:max_iters
         x_curr = x_curr - γ * (L * x_curr)
 
-        if i % (0.1 * max_iters) == 0
-            push!(losses, f(x_curr))
-        end
+        #if i % (0.01 * max_iters) == 0
+        push!(losses, f(x_curr))
+        #end
         if f(x_curr) < tol
             break
         end
@@ -80,20 +82,22 @@ function compute_trajectory_asynch(L, x0, γ, nblocks, block_size; max_iters=100
     d1 = Normal(0.1 * B, 0.01 * B)
     d2 = Normal(0.8 * B, 0.05 * B)
 
-    mixture = MixtureModel([d1, d2], [0.0, 1.0])
+    mixture = MixtureModel([d1, d2], [0.5, 0.5])
 
-    #periods = rand(mixture, nblocks)
-    #periods = ceil.(Int, periods)
-    #println(periods)
-    #phases = [rand(0:periods[i]-1) for i in 1:nblocks]
-    #broadcast_probs = rand(0:0.001:0.3, nblocks)
-    #update_probs = rand(nblocks)
+    periods = rand(mixture, nblocks)
+    periods = ceil.(Int, periods)
+    println(periods)
+    #periods = repeat([B], nblocks)
+    phases = [rand(0:periods[i]-1) for i in 1:nblocks]
+    #phases = zeros(Int, nblocks)
+    #broadcast_probs = rand(0.0:0.001:0.3, nblocks)
+    #update_probs = rand(0.5:0.001:1.0, nblocks)
 
     # Dumb test case
-    periods = [B, B]
-    phases = [0, 0]
+    #periods = [B, B]
+    #phases = [0, 0]
 
-    losses = [f(x0)]
+    traj = [x0]
 
     for t in 1:max_iters
         # Test that local states are stale i.e. columns have different values
@@ -114,57 +118,61 @@ function compute_trajectory_asynch(L, x0, γ, nblocks, block_size; max_iters=100
             # update global state
             global_state[Block(i)] = local_states[Block(i), Block(i)][:]
             #end
+            #if rand() < broadcast_probs[i]
+            # if it's the right time, broadcast your local state to other agents
             if t % periods[i] == phases[i]
-                #if rand() < broadcast_probs[i]
-                # if it's the right time, broadcast your local state to other agents
                 x = local_states[Block(i), Block(i)]
                 local_states[Block(i), :] .= x
+                # Resample your phase
+                phases[i] = rand(0:periods[i]-1)
                 #=for j in 1:nblocks
                     local_states[Block(i), Block(j)] = x 
                 end=#
             end
         end
         #println(local_states)
-        if t % (0.1 * max_iters) == 0
-            push!(losses, f(global_state))
-        end
-        #=if losses[end] < tol
+        #if t % (0.01 * max_iters) == 0
+        push!(traj, global_state)
+        #end
+        if f(traj[end]) < tol
             break
-        end=#
+        end
     end
-    return losses, global_state
+    return traj
 end
 
 
-T = Int(5e3)
-B = 30
+T = Int(1e5)
+B = 100
 
 γ_synch = 1 / K
 #γ_synch = 1
 γ_asynch = 2 / (K * (1 + 2 * (sqrt(n_agents) * B)))
 
-x0 = vcat(repeat([5.0], 4)..., repeat([15.0], 4)...)
+x0 = rand(5:0.001:15, 4 * n_agents)
 
-energies_synch, final_state_synch = compute_trajectory_asynch(L, x0, γ_synch, n_agents, 4; B=B, max_iters=T)
-energies_asynch, final_state_asynch = compute_trajectory_asynch(L, x0, γ_asynch, n_agents, 4; B=B, max_iters=T)
+@time energies_synch, final_state_synch = compute_trajectory(L, x0, γ_synch; max_iters=T)
+traj_asynch = compute_trajectory_asynch(L, x0, γ_synch, n_agents, 4; B=B, max_iters=T)
+energies_asynch = energy_function(L).(traj_asynch)
 
-plt = plot(energies_synch, title="Synch SS vs Asynch SS", xlabel="t", ylabel="Energy", label="1/K")
-plot!(plt, energies_asynch, label="2/(K(1+2√(n)B))")
+plt = plot(energies_synch, yscale=:log10, title="Synch vs Asynch", xlabel="t", ylabel="Energy", label="Synch")
+plot!(plt, energies_asynch, yscale=:log10, label="Asynch")
 
 function run_experiments(T)
     plts = []
-    for i in 1:20
+    x0 = rand(5:0.001:15, 4 * n_agents)
+    plt = plot(title="Asynch Convergence", xlabel="t", ylabel="Energy")
+    for i in 1:4
+        B = 10^(i - 1)
 
-        x0 = rand(-5:0.01:5, 4 * n_agents)
-
-        energies_synch, final_state_synch = compute_trajectory(L, x0, γ_synch; max_iters=T)
+        #energies_synch, final_state_synch = compute_trajectory(L, x0, γ_synch; max_iters=T)
         energies_asynch, final_state_asynch = compute_trajectory_asynch(L, x0, γ_synch, n_agents, 4; B=B, max_iters=T)
 
-        plt = plot(energies_synch, yscale=:log10, title="Synch vs Asynch", xlabel="t", ylabel="Energy", label="Synch")
-        plot!(plt, energies_asynch, yscale=:log10, label="Asynch")
-        push!(plts, plt)
+        plot!(plt, energies_asynch, yscale=:log10, label="B = $B")
     end
-    return plts
+    return plt
 end
+
+#plt = run_experiments(T);
+
 plt
-#plts = run_experiments(T);
